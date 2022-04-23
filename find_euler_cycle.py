5ef287ae72fbb2d8a3c2a50ab13302c8b8cc6906
Graham Strickland <grahamhstrickland@gmail.com>
2022-04-23 17:55:22 +0200

    Completed partial definition of find_euler_cycle.py

--- find_euler_cycle.py
+++ find_euler_cycle.py
@@ -1,26 +1,38 @@
 # Tucker - Applied Combinatorics 6e - Section 2.1 Ex. 20. (a)
 # Given list of tuples representing edges, find Euler cycle
 # if one exists. Output in form of list with elements representing
-# consecutive vertices in Euler cycle. Output error if no Euler
+# consecutive vertices in Euler cycle. Return empty list if no Euler
 # cycle exists.
 
-def find_euler_cycle(G: list[tuple]):
-    degree = {}
+def find_euler_cycle(V: list[int], E: list[tuple]):
+    """Return a list containing an Euler cycle in the graph represented 
+    by G = (V, E) if one exists, otherwise return empty list."""
+    degrees = {vertex: 0 for vertex in V}
+
+    # Add all degrees
+    for edge in E:
+        degrees[edge[0]] += 1
+        degrees[edge[1]] += 1
 
     # Check degree of all vertices, if not even, no Euler cycle exists
-    for edge in G:
-        if edge[0] not in degree.keys():
-            degree[edge[0]] = 1
-        else:
-            degree[edge[0]] += 1
-        if edge[1] not in degree.keys():
-            degree[edge[1]] = 1
-        else:
-            degree[edge[1]] += 1
-    if any(value % 2 != 0 for value in degree.values()):
-        print("The graph G has no Euler cycle.")
-    else:   # Find cycle and add to list
-        cycle = []
-        vertex = G[0][0]
-        
+    if any(value % 2 != 0 for value in degrees.values()):
+        return []
+    else:   # Find Euler cycle
+        e_cycle = []
+        curr_edge = E[0]
+        E.remove(curr_edge)
+        e_cycle.append(curr_edge[0])
+        e_cycle.append(curr_edge[1])
+        while len(E) > 0:
+            for edge in E:
+                if curr_edge[1] in edge:
+                    if curr_edge[1] == edge[0]:
+                        e_cycle.append(curr_edge[0])
+                        e_cycle.append(curr_edge[1])
+                    else:
+                        e_cycle.append(curr_edge[1])
+                        e_cycle.append(curr_edge[0])
+                    curr_edge = edge
+                    E.remove(curr_edge)
 
+        return e_cycle
--- tests/test_find_euler_cycle.py
+++ tests/test_find_euler_cycle.py
@@ -3,7 +3,30 @@ from nose.tools import assert_equal
 from ..find_euler_cycle import find_euler_cycle
 
 def test_find_euler_cycle0():
-    G = [(1,2), (2,1)]
-    exp = [1,2]
-    obs = find_euler_cycle(G)
+    V = [1,2]
+    E = [(1,2), (2,1)]
+    exp = [1,2,2,1]
+    obs = find_euler_cycle(V, E)
+    assert_equal(exp, obs)
+
+def test_find_euler_cycle1():
+    V = [1,2,3]
+    E = [(1,2), (2,3)]
+    exp = []
+    obs = find_euler_cycle(V, E)
+    assert_equal(exp, obs)
+
+def test_find_euler_cycle2():
+    V = ['A','B','C','D']
+    E = [('A','B'),('B','A'),('A','C'),('B','C'),
+            ('B','D'),('D','B'),('C','D')]
+    exp = []
+    obs = find_euler_cycle(V, E)
+    assert_equal(exp, obs)
+
+def test_find_euler_cycle3():
+    V = ['A','B','C','D']
+    E = [('A','B'),('A','C'),('B','D'),('C','D')]
+    exp = ['A','B','D','C','A']
+    obs = find_euler_cycle(V, E)
     assert_equal(exp, obs)
